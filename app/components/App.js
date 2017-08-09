9bbf8fbacce7678f015d051a98fc35a9f6cb453b
mattgaskey <mattgaskey@gmail.com>
2017-08-09 15:38:50 +0300

    Power on/off function added; CSS animation test; counter added; lower-level components refactored as SFCs

--- app/components/App.js
+++ app/components/App.js
@@ -5,42 +5,48 @@ var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
 var Switch = ReactRouter.Switch;
 
-class GridBlock extends React.Component {
-	constructor(props) {
-		super(props);
+function GridBlock(props) {
 
-		this.handleClick = this.handleClick.bind(this);
+	var handleClick = function() {
+		props.playerMove(props.color);
 	}
 
-	handleClick() {
-		this.props.playerMove(this.props.color);
+	var componentClasses = ['grid-block', props.color];
+	if (props.power) {
+		componentClasses.push('flash');
 	}
 
-	render() {
-		return(
-			<div 
-				className={'grid-block ' + this.props.color}
-				onClick={this.handleClick}
-			>
-			</div>
-		)
-	}
+	return(
+		<div 
+			className={ componentClasses.join(' ') }
+			onClick={handleClick}
+			id={props.color}
+		>
+		</div>
+	)
 }
 
-class Controls extends React.Component {
-	constructor(props) {
-		super(props);
 
-
+function Controls(props) {
+	
+	var powerButton = function() {
+		if (!props.power) {
+			props.powerUp();
+		}
+		if (props.power) {
+			props.powerDown();
+		}
 	}
 
-	render() {
-		return(
-			<div className='controls'>
-				Simon!
+	return(
+		<div className='controls'>
+			<div className='controls-relative-container'>
+				<div className='simon'>Simon!</div>
+				<div className='counter'>{props.round}</div>
+				<div className='power' onClick={powerButton}>Power</div>
 			</div>
-		)
-	}
+		</div>
+	)
 }
 
 class App extends React.Component {
@@ -49,12 +55,16 @@ class App extends React.Component {
 
 		this.state = {
 			grid: ['red', 'blue', 'yellow', 'green'],
-			round: 1,
+			round: 0,
 			matchSequence: [],
-			playerSequence: []
+			playerSequence: [],
+			power: false,
+			initState: false
 		}
 
 		this.updatePlayerSequence = this.updatePlayerSequence.bind(this);
+		this.powerDown = this.powerDown.bind(this);
+		this.powerUp = this.powerUp.bind(this);
 	}
 
 	updatePlayerSequence(move) {
@@ -68,14 +78,39 @@ class App extends React.Component {
 		});
 	}
 
+	powerDown() {
+		this.setState(function() {
+			return {
+				round: 0,
+				matchSequence: [],
+				playerSequence: [],
+				power: false
+			}
+		});
+	}
+
+	powerUp() {
+		this.setState(function() {
+			return {
+				round: 1,
+				power: true
+			}
+		});
+	}
+
 	render() {
 		return (
 			<div className='container'>
-				<GridBlock color='red' playerMove={this.updatePlayerSequence}/>
-				<GridBlock color='blue' playerMove={this.updatePlayerSequence}/>
-				<GridBlock color='yellow' playerMove={this.updatePlayerSequence}/>
-				<GridBlock color='green' playerMove={this.updatePlayerSequence}/>
-				<Controls />
+				<GridBlock color='red' playerMove={this.updatePlayerSequence} power={this.state.power}/>
+				<GridBlock color='blue' playerMove={this.updatePlayerSequence} power={this.state.power}/>
+				<GridBlock color='green' playerMove={this.updatePlayerSequence} power={this.state.power}/>
+				<GridBlock color='yellow' playerMove={this.updatePlayerSequence} power={this.state.power}/>
+				<Controls 
+					round={this.state.round}
+					power={this.state.power}
+					powerUp={this.powerUp}
+					powerDown={this.powerDown}
+				/>
 			</div>
 		)
 	}
